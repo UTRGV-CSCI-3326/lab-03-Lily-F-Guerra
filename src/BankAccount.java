05a664f4d1b544c2440b4594c30430683d77de14
Lily F Guerra <115589260+Lily-F-Guerra@users.noreply.github.com>
2025-01-30 20:44:51 +0000

    First commit. Had to get some help on rounding off the extra digits for the final number.

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,14 @@
+public class BankAccount{
+    public static void main(String[] args){
+        float account = (float) 2175.37;
+        account -= 302.50;
+        account += 50.03;
+        account /= 2;
+        account += 20.00;
+        account -= 1;
+        account *= 2;
+        account += 1;
 
+       System.out.println("Bob's final account balance is " + String.format("%.2f", account));
+    }
+}
